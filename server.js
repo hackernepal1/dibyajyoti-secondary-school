f359e7f93c04967e74bdaa271442da0e97b31ff4
hackernepal1 <156161564+hackernepal1@users.noreply.github.com>
2026-09-07 00:12:53 +0545

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ const Event = require("./Event");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const FRONTEND_DIR = path.join(__dirname);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 if (!process.env.MONGODB_URI) throw new Error("MONGODB_URI is required");
 if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is required");
